bfa0c106b45fe0cd3ffecd2cc791e85bede4c212
Hascher16 <hmlk16@thm.de>
2025-05-08 18:41:31 +0200

    Update home_page.dart Schrift zentrieren

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key}); // const-Konstruktor
+  const HomePage({super.key}); 
 
   @override
   Widget build(BuildContext context) {
@@ -10,10 +10,13 @@ class HomePage extends StatelessWidget {
         title: Text('Portfolio von Hascher Malik'),
       ),
       body: Center(
-        child: Text(
-          'Willkommen im Portfolio von Hascher Malik',
-          style: TextStyle(
-            fontSize: 30,  // Ändere hier die Schriftgröße
+        child: Padding(
+          padding: const EdgeInsets.all(16.0), 
+          child: Text(
+            'Willkommen im Portfolio von Hascher Malik',
+            style: TextStyle(
+              fontSize: 30, 
+            ),
           ),
         ),
       ),
