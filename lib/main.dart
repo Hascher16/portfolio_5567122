61979e84cf61a9068335dff7a386ebd9217fc261
Hascher16 <hmlk16@thm.de>
2025-06-20 02:12:32 +0200

    Summary eingefügt

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,6 @@ import 'pages/profile_form_page.dart';
 import 'pages/settings_page.dart';
 import 'pages/summary_page.dart';
 
-
-
 void main() {
   runApp(const MyApp());
 }
@@ -22,23 +20,21 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const HomePage(),
-	routes: {
-  '/slider': (context) => const SliderPage(),
-  '/profile': (context) => const ProfileFormPage(),
-  '/settings': (context) => const SettingsPage(),
-  '/summary': (context) => const SummaryPage(
-    name: '',
-    email: '',
-    about: '',
-    sliderValue: 0,
-    newsletter: false,
-    notifications: false,
-    darkMode: false,
-    offlineMode: false,
-  ),
-},
-
-	},
+      routes: {
+        '/slider': (context) => const SliderPage(),
+        '/profile': (context) => const ProfileFormPage(),
+        '/settings': (context) => const SettingsPage(),
+        '/summary': (context) => SummaryPage(
+          name: '',
+          email: '',
+          about: '',
+          sliderValue: 0,
+          newsletter: false,
+          notifications: false,
+          darkMode: false,
+          offlineMode: false,
+        ),
+      },
     );
   }
 }
